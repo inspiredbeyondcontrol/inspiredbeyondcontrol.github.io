29992fd33374d024e8f1e3d106d0cc4ef9558952
Chris Adams <inspired.beyondcontrol@gmail.com>
2020-04-10 14:43:08 -0700

    shrink pic file sizes, add responsive for about, add animination to email button under Who am I in about site, other changes

--- resources/js/changeMyPic.js
+++ resources/js/changeMyPic.js
@@ -1,9 +1,9 @@
 let myPic = document.getElementById("myPic");
 
 myPic.addEventListener("mouseenter", function(){
-  myPic.src = "resources/pics/me_working_vivid.JPG";
+  myPic.src = "resources/pics/me_working_vivid.jpeg";
 });
 
 myPic.addEventListener("mouseout", function(){
-  myPic.src = "resources/pics/me_working.JPG";
+  myPic.src = "resources/pics/me_working.jpeg";
 });
